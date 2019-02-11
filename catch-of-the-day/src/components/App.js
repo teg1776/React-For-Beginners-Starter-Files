20308eea0c571f71896af20db426d134326f8317
teg1776 <teg1776@gmail.com>
2019-02-10 18:13:44 -0600

    added components and will update state with form submit

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+
+class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    const fishes = { ...this.state.fishes };
+    fishes[`fish${Date.now()}`] = fish;
+    this.setState({
+      
+    })
+  };
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+        </div>
+        <Order />
+        <Inventory addFish={this.addFish} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Order extends React.Component {
+  render() {
+    return <div className="order">Orders go here</div>;
+  }
+}
+
+export default Order;
