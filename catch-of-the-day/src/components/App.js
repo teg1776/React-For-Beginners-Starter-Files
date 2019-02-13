a53ff0875ae209b797a63c4957a06ada396df3d4
teg1776 <teg1776@gmail.com>
2019-02-12 19:14:54 -0600

    can now update state.order with add to order button

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -12,8 +12,13 @@ class App extends React.Component {
   };
 
   addFish = fish => {
+    // 1. take copy of existing state
     const fishes = { ...this.state.fishes };
+
+    // 2. add our new fish to the fishes variable
     fishes[`fish${Date.now()}`] = fish;
+
+    // 3. set the new fishes object to state
     this.setState({
       fishes
     });
@@ -25,6 +30,17 @@ class App extends React.Component {
     });
   };
 
+  addToOrder = key => {
+    // 1. take a copy of the state
+    const order = { ...this.state.order };
+
+    // 2. either add to the order, or update the number in our order
+    order[key] = order[key] + 1 || 1;
+
+    // 3. call setState to update our state object
+    this.setState({ order });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -32,7 +48,12 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
         </div>
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -5,7 +5,7 @@ class Inventory extends React.Component {
   render() {
     return (
       <div className="inventory">
-        <h2>Inventory!!</h2>
+        <h2>Inventory</h2>
         <AddFishForm addFish={this.props.addFish} />
         <button onClick={this.props.loadSampleFishes}>
           Load Sample Fishes
