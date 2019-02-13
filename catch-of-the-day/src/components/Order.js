4eca6ff74114d58f9cfe91160168ee0f429790c7
teg1776 <teg1776@gmail.com>
2019-02-12 19:30:20 -0600

    am rendering appropriate totals

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,8 +1,32 @@
 import React from "react";
+import { formatPrice } from "../helpers";
 
 class Order extends React.Component {
   render() {
-    return <div className="order">Orders go here</div>;
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvaialbe = fish && fish.status === "available";
+      if (isAvaialbe) {
+        return prevTotal + count * fish.price;
+      }
+      return prevTotal;
+    }, 0);
+    return (
+      <div className="order-wrap">
+        <h2>Your Order</h2>
+        <ul>
+          {orderIds.map(key => (
+            <li>{key}</li>
+          ))}
+        </ul>
+        <div className="total">
+          Total:
+          <strong>{formatPrice(total)}</strong>
+        </div>
+      </div>
+    );
   }
 }
 
